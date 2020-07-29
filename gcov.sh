974a8d47155df704ac4a82f474e4371e3a6bc380
pordaniel01 <pordaniel40@gmail.com>
2020-07-29 15:20:49 +0200

    #8 fixed gcov.sh bug

--- gcov.sh
+++ gcov.sh
@@ -2,8 +2,8 @@
 #These commands move the required coverage source to the place where gcov searhes for them
 
 
-mv $TRAVIS_BUILD_DIR/build/test/CMakeFiles/RunaphysTests.dir/test.cpp.gcno $TRAVIS_BUILD_DIR/test
-mv $TRAVIS_BUILD_DIR/build/test/CMakeFiles/RunaphysTests.dir/test.cpp.gcda $TRAVIS_BUILD_DIR/test
+mv $TRAVIS_BUILD_DIR/build/test/CMakeFiles/RunaphysTests.dir/tests.cpp.gcno $TRAVIS_BUILD_DIR/test
+mv $TRAVIS_BUILD_DIR/build/test/CMakeFiles/RunaphysTests.dir/tests.cpp.gcda $TRAVIS_BUILD_DIR/test
 mv $TRAVIS_BUILD_DIR/build/src/CMakeFiles/Runaphys.dir/critical_field.cpp.gcno $TRAVIS_BUILD_DIR/src
 mv $TRAVIS_BUILD_DIR/build/src/CMakeFiles/Runaphys.dir/critical_field.cpp.gcda $TRAVIS_BUILD_DIR/src
 mv $TRAVIS_BUILD_DIR/build/src/CMakeFiles/Runaphys.dir/growth_rate.cpp.gcda $TRAVIS_BUILD_DIR/src
