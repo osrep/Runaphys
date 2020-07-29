c92ecfd747e56acc182d8a908fe74b8a969e1cc0
pordaniel01 <pordaniel40@gmail.com>
2020-07-29 15:14:48 +0200

    #8 modified gcov.sh file

--- gcov.sh
+++ gcov.sh
@@ -2,11 +2,11 @@
 #These commands move the required coverage source to the place where gcov searhes for them
 
 
-mv $TRAVIS_BUILD_DIR/build/test/CMakeFiles/RuninTests.dir/test.cpp.gcno $TRAVIS_BUILD_DIR/test
-mv $TRAVIS_BUILD_DIR/build/test/CMakeFiles/RuninTests.dir/test.cpp.gcda $TRAVIS_BUILD_DIR/test
-mv $TRAVIS_BUILD_DIR/build/src/CMakeFiles/Runin.dir/critical_field.cpp.gcno $TRAVIS_BUILD_DIR/src
-mv $TRAVIS_BUILD_DIR/build/src/CMakeFiles/Runin.dir/critical_field.cpp.gcda $TRAVIS_BUILD_DIR/src
-mv $TRAVIS_BUILD_DIR/build/src/CMakeFiles/Runin.dir/growth_rate.cpp.gcda $TRAVIS_BUILD_DIR/src
-mv $TRAVIS_BUILD_DIR/build/src/CMakeFiles/Runin.dir/growth_rate.cpp.gcno $TRAVIS_BUILD_DIR/src
+mv $TRAVIS_BUILD_DIR/build/test/CMakeFiles/RunaphysTests.dir/test.cpp.gcno $TRAVIS_BUILD_DIR/test
+mv $TRAVIS_BUILD_DIR/build/test/CMakeFiles/RunaphysTests.dir/test.cpp.gcda $TRAVIS_BUILD_DIR/test
+mv $TRAVIS_BUILD_DIR/build/src/CMakeFiles/Runaphys.dir/critical_field.cpp.gcno $TRAVIS_BUILD_DIR/src
+mv $TRAVIS_BUILD_DIR/build/src/CMakeFiles/Runaphys.dir/critical_field.cpp.gcda $TRAVIS_BUILD_DIR/src
+mv $TRAVIS_BUILD_DIR/build/src/CMakeFiles/Runaphys.dir/growth_rate.cpp.gcda $TRAVIS_BUILD_DIR/src
+mv $TRAVIS_BUILD_DIR/build/src/CMakeFiles/Runaphys.dir/growth_rate.cpp.gcno $TRAVIS_BUILD_DIR/src
 gcov src/critical_field.cpp.cpp 
 gcov src/growth_rate.cpp.cpp
