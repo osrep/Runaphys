2b74dabf0da9f4fea1d0e832f047bff15bff2700
pordaniel01 <pordaniel40@gmail.com>
2020-07-29 14:22:07 +0200

    #8 Putting gcov commands in gcov.sh

---
+++ gcov.sh
@@ -0,0 +1,12 @@
+#script that runs gcov coverage measurments
+#These commands move the required coverage source to the place where gcov searhes for them
+
+
+mv $TRAVIS_BUILD_DIR/build/test/CMakeFiles/RuninTests.dir/test.cpp.gcno $TRAVIS_BUILD_DIR/test
+mv $TRAVIS_BUILD_DIR/build/test/CMakeFiles/RuninTests.dir/test.cpp.gcda $TRAVIS_BUILD_DIR/test
+mv $TRAVIS_BUILD_DIR/build/src/CMakeFiles/Runin.dir/critical_field.cpp.gcno $TRAVIS_BUILD_DIR/src
+mv $TRAVIS_BUILD_DIR/build/src/CMakeFiles/Runin.dir/critical_field.cpp.gcda $TRAVIS_BUILD_DIR/src
+mv $TRAVIS_BUILD_DIR/build/src/CMakeFiles/Runin.dir/growth_rate.cpp.gcda $TRAVIS_BUILD_DIR/src
+mv $TRAVIS_BUILD_DIR/build/src/CMakeFiles/Runin.dir/growth_rate.cpp.gcno $TRAVIS_BUILD_DIR/src
+gcov src/critical_field.cpp.cpp 
+gcov src/growth_rate.cpp.cpp
