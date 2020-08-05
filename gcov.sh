d3d796844e3d376e77373648e7be348eee3640e3
pordaniel01 <pordaniel40@gmail.com>
2020-08-05 13:42:55 +0200

    #33 gcov.sh updated

--- gcov.sh
+++ gcov.sh
@@ -8,5 +8,17 @@ mv ./build/src/CMakeFiles/Runaphys.dir/critical_field.cpp.gcno $TRAVIS_BUILD_DIR
 mv ./build/src/CMakeFiles/Runaphys.dir/critical_field.cpp.gcda $TRAVIS_BUILD_DIR/src
 mv ./build/src/CMakeFiles/Runaphys.dir/growth_rate.cpp.gcda $TRAVIS_BUILD_DIR/src
 mv ./build/src/CMakeFiles/Runaphys.dir/growth_rate.cpp.gcno $TRAVIS_BUILD_DIR/src
+
+mv ./build/src/CMakeFiles/Runaphys.dir/avalanche.cpp.gcno $TRAVIS_BUILD_DIR/src
+mv ./build/src/CMakeFiles/Runaphys.dir/avalanche.cpp.gcda $TRAVIS_BUILD_DIR/src
+mv ./build/src/CMakeFiles/Runaphys.dir/control.cpp.gcno $TRAVIS_BUILD_DIR/src
+mv ./build/src/CMakeFiles/Runaphys.dir/control.cpp.gcda $TRAVIS_BUILD_DIR/src
+mv ./build/src/CMakeFiles/Runaphys.dir/dreicer.cpp.gcno $TRAVIS_BUILD_DIR/src
+mv ./build/src/CMakeFiles/Runaphys.dir/dreicer.cpp.gcda $TRAVIS_BUILD_DIR/src
+
+
 gcov src/critical_field.cpp.cpp 
 gcov src/growth_rate.cpp.cpp
+gcov src/avalanche.cpp.cpp
+gcov src/control.cpp.cpp
+gcov src/dreicer.cpp.cpp
