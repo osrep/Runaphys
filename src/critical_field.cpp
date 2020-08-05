1eb147e2a28ec331a506f355e48f66539257837c
pordaniel01 <pordaniel40@gmail.com>
2020-08-05 11:17:42 +0200

    #10 Added clarification to comments

--- src/critical_field.cpp
+++ src/critical_field.cpp
@@ -9,6 +9,8 @@
  * Checks if a electric field is larger then the critical field 
  * It requires the physical parameters of the plasma, which is given in the profile
  * Above a certain rho given by rho_max, the flag is not given back
+ * regardless of the outcome of the check.
+ * Otherwise if it is larger it returns true (1) and false (0) if not
  */
 int is_field_critical(profile pro, double rho_max) {
 
--- src/growth_rate.cpp
+++ src/growth_rate.cpp
@@ -9,7 +9,9 @@
 /**
  * Checks if growth is over a specified limit, given by the limit parameter
  * It requires the physical parameters of the plasma, which is given in the profile
- * Above a certain rho given by rho_max, the flag is not given back
+ * Above a certain rho given by rho_max, a false flag is returned
+ * regardless of the outcome of the check.
+ * Otherwise if it is over the limit it returns true (1) and false (0) if not
  */
 int is_growth_rate_over_limit(profile pro, double limit, double rho_max) {
 
